ab83c3bdb0677fc2ab089f17a3745bf226de7703
LloydJr <98184266+LloydJr@users.noreply.github.com>
2022-07-11 14:20:38 -0400

    getters and setters added for rouletteBall

---
+++ src/main/java/com/github/zipcodewilmington/casino/games/roulette/rouletteBall.java
@@ -0,0 +1,32 @@
+package com.github.zipcodewilmington.casino.games.roulette;
+
+public class rouletteBall {
+
+    private int number;
+    private String color;
+    private boolean even;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isEven() {
+        return even;
+    }
+
+    public void setEven(boolean even) {
+        this.even = even;
+    }
+}
---
+++ src/main/java/com/github/zipcodewilmington/casino/games/roulette/rouletteGame.java
@@ -0,0 +1,7 @@
+package com.github.zipcodewilmington.casino.games.roulette;
+
+import java.util.Random;
+
+public class rouletteGame {
+
+}
