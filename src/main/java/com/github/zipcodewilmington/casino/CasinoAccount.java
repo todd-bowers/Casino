813eeb279cc4f1ebd1a11aa38c944a8f40c31d92
eugeneshine1 <eugeneshine1@gmail.com>
2022-07-11 14:58:44 -0400

    skeleton for casinoAccount

--- src/main/java/com/github/zipcodewilmington/casino/CasinoAccount.java
+++ src/main/java/com/github/zipcodewilmington/casino/CasinoAccount.java
@@ -6,5 +6,30 @@ package com.github.zipcodewilmington.casino;
  * The `ArcadeAccount` is used to log into the system to select a `Game` to play.
  */
 public class CasinoAccount {
-//    a
+    private Integer money;
+    private String accountName;
+    private String accountPassword;
+
+    public CasinoAccount (String accountName, String accountPassword, Integer money) {
+        this.accountName = accountName;
+        this.accountPassword = accountPassword;
+        this.money = money;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public String getAccountPassword() {
+        return accountPassword;
+    }
+
+    public void setAccountPassword(String accountPassword) {
+        this.accountPassword = accountPassword;
+    }
+
+    public int getMoney () {
+        return money;
+    }
+
 }
