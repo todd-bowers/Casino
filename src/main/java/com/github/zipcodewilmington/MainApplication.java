0fbc105f495e91ee4929ebcc4057dbfa7459290e
jd <j.judd.singer@gmail>
2022-07-15 01:37:24 -0400

    Modified Hand and Card classes, implemented Hand and Shoe instances in BlackjackGame, implemented base run logic for BlackjackGame

--- src/main/java/com/github/zipcodewilmington/MainApplication.java
+++ src/main/java/com/github/zipcodewilmington/MainApplication.java
@@ -1,30 +1,26 @@
 package com.github.zipcodewilmington;
 
 import com.github.zipcodewilmington.casino.games.blackjack.BlackjackGame;
-import com.github.zipcodewilmington.casino.games.blackjack.BlackjackPlayer;
-import com.github.zipcodewilmington.casino.games.card.Card;
-import com.github.zipcodewilmington.casino.games.card.Hand;
-import com.github.zipcodewilmington.casino.games.card.Shoe;
-import com.github.zipcodewilmington.casino.games.card.deck.Deck;
-
-import java.util.List;
 
 public class MainApplication {
     public static void main(String[] args) {
-        BlackjackGame blackjack = new BlackjackGame();
-        BlackjackPlayer player = new BlackjackPlayer();
-        BlackjackPlayer dealer = new BlackjackPlayer();
-        Deck playingDeck = new Deck();
-        Shoe shoe = new Shoe();
-        List<Card> fullShoe = shoe.getAllCards();
-        System.out.println(fullShoe.size());
-//        blackjack.setBet();
-        playingDeck.draw(dealer);
-        playingDeck.draw(player);
+        BlackjackGame bjGame = new BlackjackGame();
+//        BlackjackPlayer player = new BlackjackPlayer();
+//        BlackjackPlayer dealer = new BlackjackPlayer();
+//        Deck playingDeck = new Deck();
+//        Shoe shoe = new Shoe();
+//        List<Card> fullShoe = shoe.getAllCards();
+//        System.out.println(fullShoe.size());
+//
+////        blackjack.setBet();
+//        playingDeck.draw(Hand);
+//        playingDeck.draw(player);
+//
+//
+//
+//        blackjack.betOption();
 
-
-        blackjack.betOption();
-
+    bjGame.run();
 
     }
 }
--- src/main/java/com/github/zipcodewilmington/casino/games/blackjack/BlackjackGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/blackjack/BlackjackGame.java
@@ -2,16 +2,24 @@ package com.github.zipcodewilmington.casino.games.blackjack;
 
 import com.github.zipcodewilmington.casino.GameInterface;
 import com.github.zipcodewilmington.casino.PlayerInterface;
+import com.github.zipcodewilmington.casino.games.card.Hand;
 import com.github.zipcodewilmington.casino.games.card.Shoe;
 import com.github.zipcodewilmington.casino.games.card.deck.Deck;
 import com.github.zipcodewilmington.utils.AnsiColor;
 import com.github.zipcodewilmington.utils.IOConsole;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
 
 public class BlackjackGame extends Deck implements GameInterface {
 
+    private Hand dealersHand = new Hand();
+    private Hand playerHand = new Hand();
+    private Shoe shoe = new Shoe();
+    private Boolean isRunning = false;
+    private Boolean dealerDefaultWin = false;
+    private Boolean dealersTurn = false;
 
     private final IOConsole console = new IOConsole(AnsiColor.AUTO);
     public LinkedList<BlackjackPlayer> blackjackPlayer = new LinkedList<>();
@@ -24,6 +32,71 @@ public class BlackjackGame extends Deck implements GameInterface {
     }
 
     public void  run(){
+        this.initializeNewGame();
+
+        while(this.isRunning == true){
+            System.out.println("Successfully started game");
+            this.advanceTurn();
+            this.isRunning = false;
+        }
+
+//        this.determineWinner();
+//        System.out.println((this.printEndingMessage()));
+
+    }
+
+    private void advanceTurn() {
+        if(this.dealersTurn){
+
+        } else {
+            //player's code
+            console.println("Player's Turn. Current Hand:");
+            System.out.println(this.playerHand.toString());
+            if(this.console.getIntegerInput("Please enter your choice\n1: Hit\n2: Stand") == 1){
+                this.playerHand.addCard(shoe.drawNext());
+                System.out.println("Added card");
+            } else {
+                //if something stand related needs to happen, set up here?
+                System.out.println("chose to stand");
+            }
+        }
+
+        this.dealersTurn = !this.dealersTurn;
+    }
+
+    public void initializeNewGame() {
+        this.initializeHands();
+        this.dealInitialHands();
+        this.checkDealerWinDefault();
+        this.isRunning = true;
+
+    }
+
+    private void checkDealerWinDefault() {
+        this.dealerDefaultWin = false;
+
+        if(this.dealersHand.getCards().get(0).getRank().getRankName().equals("Ace")){
+            this.dealerDefaultWin = this.dealersHand.getCards().get(1).getRank().getRankValue() == 10;
+        }
+    }
+
+    private void dealInitialHands() {
+        System.out.println("Hitting dealInitial Hands");
+        this.playerHand.addCard(shoe.drawNext());
+        this.dealersHand.addCard(shoe.drawNext());
+        this.playerHand.addCard(shoe.drawNext());
+        this.dealersHand.addCard(shoe.drawNext());
+        this.dealersHand.getCards().get(1).setFaceUp(true);
+    }
+
+    public void initializeHands() {
+        System.out.println("Hitting initialize hands");
+        this.dealersHand = new Hand();
+        this.playerHand = new Hand();
+    }
+
+    public String printEndingMessage() {
+        return "Game Over";
     }
 
     public void blackjack(){
