555b42ceafd50fbb7bc4c3ea55d12a917e2c53db
jd <j.judd.singer@gmail>
2022-07-15 16:08:17 -0400

    sort of working merger errors

--- src/main/java/com/github/zipcodewilmington/MainApplication.java
+++ src/main/java/com/github/zipcodewilmington/MainApplication.java
@@ -1,14 +1,14 @@
 package com.github.zipcodewilmington;
-import com.github.zipcodewilmington.casino.games.roulette.RouletteBall;
-import com.github.zipcodewilmington.casino.games.roulette.RouletteGame;
 
-import java.util.Random;
+import com.github.zipcodewilmington.casino.games.blackjack.BlackjackGame;
 
 public class MainApplication {
     public static void main(String[] args) {
-        new Casino().run();
-        RouletteGame game = new RouletteGame();
-        game.run();
-//        new Casino().run();
+        BlackjackGame bjGame = new BlackjackGame();
+//        BlackjackPlayer player = new BlackjackPlayer();
+
+
+        bjGame.run();
+
     }
 }
