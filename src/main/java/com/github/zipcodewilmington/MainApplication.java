91f21c3da47880d37c7b661cac04c598e6f019c1
LloydJr <98184266+LloydJr@users.noreply.github.com>
2022-07-15 11:38:56 -0400

    Finished the game i think, will revise

--- src/main/java/com/github/zipcodewilmington/MainApplication.java
+++ src/main/java/com/github/zipcodewilmington/MainApplication.java
@@ -7,8 +7,6 @@ import java.util.Random;
 
 public class MainApplication {
     public static void main(String[] args) {
-        RouletteGame game = new RouletteGame();
-        game.run();
-//        new Casino().run();
+        new Casino().run();
     }
 }
