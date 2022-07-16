8aca04874acaba5ecf247be92cfdd81342b78934
eugeneshine1 <eugeneshine1@gmail.com>
2022-07-15 21:32:06 -0400

    new game added war

--- src/main/java/com/github/zipcodewilmington/MainApplication.java
+++ src/main/java/com/github/zipcodewilmington/MainApplication.java
@@ -3,14 +3,13 @@ package com.github.zipcodewilmington;
 
 import com.github.zipcodewilmington.casino.games.roulette.RouletteBall;
 import com.github.zipcodewilmington.casino.games.roulette.RouletteGame;
+import com.github.zipcodewilmington.casino.games.war.WarGame;
 
 import java.util.Random;
 
 public class MainApplication {
     public static void main(String[] args) {
         new Casino().run();
-        RouletteGame game = new RouletteGame();
-        game.run();
-//        new Casino().run();
+
     }
 }
