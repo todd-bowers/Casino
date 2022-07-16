f5e10d9288df725a8d3dcbdf87ff289328551b22
jd <j.judd.singer@gmail>
2022-07-16 12:45:49 -0400

    some changes to dealer logic and card value

--- src/main/java/com/github/zipcodewilmington/casino/games/blackjack/BlackjackGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/blackjack/BlackjackGame.java
@@ -14,6 +14,9 @@ import java.util.Scanner;
 
 public class BlackjackGame extends Deck implements GameInterface {
 
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_Blue = "\u001b[34m";
     private Hand dealersHand = new Hand();
     private Hand playerHand = new Hand();
     private Shoe shoe = new Shoe();
@@ -23,7 +26,7 @@ public class BlackjackGame extends Deck implements GameInterface {
 
     private final IOConsole console = new IOConsole(AnsiColor.AUTO);
     public LinkedList<BlackjackPlayer> blackjackPlayer = new LinkedList<>();
-
+    private boolean dealerWin;
 
 
     public void add(PlayerInterface player) {
@@ -33,15 +36,15 @@ public class BlackjackGame extends Deck implements GameInterface {
 
     public void  run(){
         this.initializeNewGame();
-
+        checkDealerWinDefault();
         while(this.isRunning == true){
-            System.out.println("Successfully started game");
+            System.out.println(ANSI_YELLOW  + "Successfully started game");
 
 //            this.setBet();
             this.advanceTurn();
-            this.checkwinner();
-            this.isRunning = false;
         }
+
+        this.checkwinner();
 
 //        this.determineWinner();
 //        System.out.println((this.printEndingMessage()));
@@ -49,33 +52,36 @@ public class BlackjackGame extends Deck implements GameInterface {
     }
 
     private void advanceTurn() {
-        if(this.dealersTurn){
-            checkDealerWinDefault();
 
+        if(this.dealersTurn){
+
+            System.out.println(this.dealersHand.toString());
+            this.checkDealer();
         } else {
             //player's code
-            console.println("Player's Turn. Current Hand:");
-            System.out.println(this.playerHand.toString());
-            System.out.println(playerHand.getHandSum());
-            int userInput = 1;
-            while(userInput == 1){
+            Integer userInput = 0;
+            while(userInput != 2 && this.isRunning){
+                console.println(ANSI_Blue + "Player's Turn. Current Hand:");
+                System.out.println(ANSI_Blue + this.playerHand.toString());
+                System.out.println(ANSI_Blue + "Hand value: " + playerHand.getHandSum() + "\n");
                 userInput = this.console.getIntegerInput("Please enter your choice\n1: Hit\n2: Stand");
                 if(userInput == 1){
                     this.playerHand.addCard(shoe.drawNext());
                     System.out.println("Added card");
-                    System.out.println(this.playerHand.toString());
 
                     this.busted();
                 } else if (userInput == 2) {
+//                    System.out.println("_______Player = 2--------");
                     this.dealersTurn = true;
-                    console.println("Dealers Turn. Current Hand:");
-                    System.out.println(this.dealersHand.toString());
+                    console.println(ANSI_RED + "Dealers Turn. Current Hand:");
+                    System.out.println(ANSI_RED + this.dealersHand.toString());
+//                    System.out.println("-----___display dealer hand__ just ran-----");
+                    System.out.println(ANSI_RED + "Dealer has: " + this.dealersHand.getHandSum());
                     this.checkDealer();
+                    System.out.println("---");
+                    System.out.println("-------End of round-----");
+                    this.isRunning = false;
                 }
-
-
-
-
                 //if something stand related needs to happen, set up here?
 //                System.out.println("chose to stand");
             }
@@ -89,17 +95,18 @@ public class BlackjackGame extends Deck implements GameInterface {
     public void initializeNewGame() {
         this.initializeHands();
         this.dealInitialHands();
-        this.checkDealerWinDefault();
-        this.isRunning = true;
+        this.isRunning = !this.checkDealerWinDefault();
 
     }
 
-    private void checkDealerWinDefault() {
-        this.dealerDefaultWin = false;
+    private Boolean checkDealerWinDefault() {
+        Boolean dealerDefaultWin = false;
 
         if(this.dealersHand.getCards().get(0).getRank().getRankName().equals("Ace")){
-            this.dealerDefaultWin = this.dealersHand.getCards().get(1).getRank().getRankValue() == 10;
+            dealerDefaultWin = this.dealersHand.getCards().get(1).getRank().getRankValue() == 10;
         }
+        this.dealerDefaultWin = dealerDefaultWin;
+        return dealerDefaultWin;
     }
 
     private void dealInitialHands() {
@@ -120,9 +127,12 @@ public class BlackjackGame extends Deck implements GameInterface {
     public void busted() {
         if (this.playerHand.getHandSum() > 21) {
             System.out.println("you went over 21, and lost your bet ");
-            isRunning = false;
+            System.out.println(this.playerHand.toString());
+            this.dealerWin = true;
+            this.isRunning = false;
         }
     }
+
 
     public String printEndingMessage() {
         return "Game Over";
@@ -149,7 +159,10 @@ public class BlackjackGame extends Deck implements GameInterface {
 //    }
     public void checkwinner(){
         int winner;
-        if(playerHand.getHandSum() > dealersHand.getHandSum()){
+        if(this.dealerDefaultWin) {
+            //Pay the player and tell them
+            System.out.println("Dealer won by default");
+        } else if(playerHand.getHandSum() > dealersHand.getHandSum()){
             System.out.println( "Player1 wins");
         } else if (playerHand.getHandSum() == dealersHand.getHandSum()){
             System.out.println("tie");
@@ -160,21 +173,29 @@ public class BlackjackGame extends Deck implements GameInterface {
     }
 
     public void checkDealer() {
-        console.println("Dealers Turn. Current Hand:");
-        System.out.println(this.dealersHand.toString());
-        while (dealersHand.getHandSum() < 17) {
+//        console.println("Dealers Turn. Current Hand:");
+        Boolean check = true;
+        while (check) {
             if (dealersHand.getHandSum() < 17) {
-                System.out.println("dealer must hit");
+                System.out.println(ANSI_RED + "------HAND IS LESS THAN 17-------\n");
+                System.out.println(ANSI_RED + "dealer must hit");
                 dealersHand.addCard(shoe.drawNext());
                 System.out.println(dealersHand.toString());
-            } else if (dealersHand.getHandSum() > 21) {
+//                System.out.println("_____dealers hand ran_____\n next is 18-20");
+            } else if (dealersHand.getHandSum() < 21 && dealersHand.getHandSum() > 17) {
+                System.out.println(ANSI_RED + "Dealer has " + dealersHand.getHandSum());
+                check = false;
 //            give player choice to pay again && pay the player
             } else if (dealersHand.getHandSum() == playerHand.getHandSum()) {
                 System.out.println("Tie");
+                check = false;
 //            replay game method here
-            } else if (dealersHand.getHandSum() > playerHand.getHandSum()) {
-                console.println("Dealers Turn. Current Hand:");
-                System.out.println(this.dealersHand.toString());
+//            } else if (dealersHand.getHandSum() > playerHand.getHandSum()) {
+//                console.println("Dealers Turn. Current Hand:");
+//                System.out.println(this.dealersHand.toString());
+            } else if (dealersHand.getHandSum() > 21){
+                System.out.println(ANSI_RED + " Dealer busted!!");
+                check = false;
 
             }
         }
